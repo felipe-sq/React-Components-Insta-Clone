62b062e88acb5736fdd2b5e03492c461c1a9005d
Felipe Quintero <ffquint@gmail.com>
2020-12-19 14:16:17 -0500

    Still working on code for Posts and App. Having an issue with passing the right props from parent to Posts and Post.

--- src/App.js
+++ src/App.js
@@ -7,17 +7,22 @@
 // Import the state hook
 import React, { useState } from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
-import './components/Posts/Posts';
-import './components/SearchBar/SearchBar';
+import Posts from './components/Posts/Posts';
+import SearchBar from './components/SearchBar/SearchBar';
 // Import the dummyData
-import './dummy-data';
+// import './dummy-data';
 import './App.css';
+import dummyData from './dummy-data';
+import Post from './components/Posts/Post';
+// console.log()
 
 const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
+  const [posts, setPosts] = useState(dummyData)
+  console.log(posts)
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
-
+  const [searchTerm, setSearchTerm] = useState('');
   const likePost = postId => {
     /*
       This function serves the purpose of increasing the number of likes by one, of the post with a given id.
@@ -30,11 +35,20 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
+    setPosts(posts.map( id => {
+      if (id === postId){
+        return { ...Post }
+      } else {
+        return { Post }
+      }
+    }))
   };
 
   return (
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
+      <SearchBar setSearchTerm={setSearchTerm}/>
+      <Posts />
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
   );
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -4,12 +4,17 @@ import './Posts.css';
 
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
-  const { likePost, posts } = props;
+  const { posts, likePost } = props;
+  console.log(posts);
 
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
+      {
+        // posts.map(post => <Post key={post.id} post={post}/>)
+      }
       {/* Check the implementation of Post to see what props it requires! */}
+      {/* <Post /> */}
     </div>
   );
 };
