4cc33060dd0a85afc785fd3985c9ef43ff2d3a0a
Felipe Quintero <ffquint@gmail.com>
2021-02-25 20:35:46 -0500

    Completed code for Comments and Post component files.

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -4,12 +4,18 @@ import './Posts.css';
 
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
-  const { likePost, posts } = props;
+  const { posts, likePost } = props;
+  // console.log(posts);
 
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
+      {
+        posts.map(post => <Post key={post.id} likePost={likePost} post={post}/>)
+      }
       {/* Check the implementation of Post to see what props it requires! */}
+      {/* <Post posts={posts}/> */}
+      {/* {console.log(Post)} */}
     </div>
   );
 };
